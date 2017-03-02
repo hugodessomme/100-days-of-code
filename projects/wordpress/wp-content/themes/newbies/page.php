843378cba51802c54dbff2d541ddfeb9debbd48d
Hugo Dessomme <hdessomme@umanit.fr>
2017-03-02 21:22:24 +0100

    Dynamise les templates de post / page / sidebar / menu

--- projects/wordpress/wp-content/themes/newbies/page.php
+++ projects/wordpress/wp-content/themes/newbies/page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <div class="row">
-    <div class="col-sm-8 blog-main">
-				
+    <div class="col-sm-12">
+		
 		<?php 
 			if( have_posts() ) : while ( have_posts() ) : the_post();
         	
@@ -11,10 +11,7 @@
     		endwhile; endif; 
 		?>
 
-    </div><!-- /.blog-main -->
-
-    <?php get_sidebar(); ?>
-
+    </div><!-- /.col -->
 </div><!-- /.row -->
 
 <?php get_footer(); ?>
