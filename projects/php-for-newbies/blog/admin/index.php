2732436a015d8382946df5bc25d5dfd496bad07b
Hugo Dessomme <hdessomme@umanit.fr>
2017-05-31 20:16:47 +0200

    Ajoute l'authetification à l'admin par htaccess

--- projects/php-for-newbies/blog/admin/index.php
+++ projects/php-for-newbies/blog/admin/index.php
@@ -3,23 +3,24 @@
 	$title = 'Accueil - Admin Blog';
 	include '../templates/header.php';
 ?>
-	<p><a href="../index.php">Retour à la liste des articles</a> - <a href="add.php">Ajouter un article</a></p>
 
-	<?php
-		include '../inc/bdd.php';
+<p><a href="../index.php">Retour à la liste des articles</a> - <a href="add.php">Ajouter un article</a></p>
 
-		// Récupère tous les articles
-		$query = $bdd->query('SELECT id, titre FROM blog_post ORDER BY date_creation DESC');
+<?php
+	include '../inc/bdd.php';
 
-		// Affichage d'un article
-		echo '<h2>Tous les articles</h2>';
-		echo '<ul>';
-		while( $data = $query->fetch() ) {
-			include '../templates/admin/post-settings.php';
-		}
-		echo '</ul>';
-		$query->closeCursor();
-	?>
+	// Récupère tous les articles
+	$query = $bdd->query('SELECT id, titre FROM blog_post ORDER BY date_creation DESC');
+
+	// Affichage d'un article
+	echo '<h2>Tous les articles</h2>';
+	echo '<ul>';
+	while( $data = $query->fetch() ) {
+		include '../templates/admin/post-settings.php';
+	}
+	echo '</ul>';
+	$query->closeCursor();
+?>
 
 <?php
 	// Footer
