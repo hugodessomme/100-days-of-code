0dd1b7e594adc236e019c651810eb0ea8a5d3c7f
Hugo Dessomme <hdessomme@umanit.fr>
2017-08-03 21:06:18 +0200

    Ajoute le modèle MVC

---
+++ projects/php-for-newbies/mvc/modele/connexion_sql.php
@@ -0,0 +1,7 @@
+<?php
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=github-php-for-newbies;charset=utf8', 'root', 'root');
+
+} catch(Exception $e) {
+	die('Erreur : ' . $e->getMessage());
+}
