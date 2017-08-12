772086826bbbd85df83eab727d10f3c0086135ff
Hugo <hdessomme49@gmail.com>
2017-08-12 22:15:42 +0200

    Ajoute le listing des articles

---
+++ projects/wordpress/wp-content/themes/newbies/functions.php
@@ -0,0 +1,36 @@
+<?php
+define('NEWBIES_VERSION', '1.0.0');
+
+// Chargement des styles / scripts côté front
+function newbies_scripts() {
+	// Styles
+	wp_enqueue_style('newbies_bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), NEWBIES_VERSION, 'all');
+	wp_enqueue_style('newbies_main', get_template_directory_uri() . '/style.css', array('newbies_bootstrap'), NEWBIES_VERSION, 'all');
+
+	// Scripts
+	wp_enqueue_script('newbies_bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), NEWBIES_VERSION, true); // false: wp_head() VS true: wp_footer()
+	wp_enqueue_script('newbies_main_js', get_template_directory_uri() . '/js/main.js', array('jquery', 'newbies_bootstrap_js'), NEWBIES_VERSION, true); // false: wp_head() VS true: wp_footer()
+}
+add_action('wp_enqueue_scripts', 'newbies_scripts');
+
+// Chargement des styles / scripts côté back
+function newbies_admin_scripts() {
+	wp_enqueue_style('newbies_admin_bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), NEWBIES_VERSION, 'all');
+}
+// add_action('admin_init', 'newbies_admin_scripts'); A DECOMMENTER POUR TESTER
+
+// Utilitaires
+function newbies_setup() {
+	// Active l'image à la une
+	add_theme_support('post-thumbnails');
+
+	// Supprime le numéro de version du head
+	remove_action('wp_head', 'wp_generator');
+
+	// Supprime les guillemets à la française
+	remove_filter('the_content', 'wptexturize');
+
+	// Affiche le title dans le head
+	add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'newbies_setup');
--- projects/wordpress/wp-content/themes/newbies/index.php
+++ projects/wordpress/wp-content/themes/newbies/index.php
@@ -1,1 +1,59 @@
-<?php echo "Hello World!"; ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" conten="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, delectus?">
+	<?php wp_head(); ?>
+</head>
+
+<body>
+	<header>
+		<nav class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Project name</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="./">Active</a></li>
+						<li><a href="../navbar-fixed-top/">Lien</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
+	</header>
+
+	<div class="container">
+		<?php if( have_posts() ): ?>
+					
+			<?php while( have_posts() ): the_post(); ?>
+				<div class="row">
+					<div class="col-xs-2">
+						<?php the_post_thumbnail('thumbnail', ['class' => 'img-responsive']); ?>
+					</div>
+					<div class="col-xs-10">
+						<h1><?php the_title(); ?></h1>
+						<p><?php the_excerpt(); ?></p>
+					</div>
+				</div>
+			<?php endwhile; ?>
+
+		<?php else: ?>
+			<div class="row">
+				<div class="col-xs-12">
+					<p>Pas de résultats</p>
+				</div>
+			</div>
+		<?php endif; ?>
+	</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
