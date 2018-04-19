ce945ce5dcace492745604023006954099f1c49b
Hugo Dessomme <hdessomme@umanit.fr>
2018-04-19 20:35:51 +0200

    [crud] : Ajoute les fonctionnalités de récupération des tâches, d'une tâche, et des catégories

--- projects/php/practise/crud/TaskManager.php
+++ projects/php/practise/crud/TaskManager.php
@@ -50,17 +50,53 @@ class TaskManager
 
   /**
    * Return a task
+   *
+   * @param int $data
+   * @return Task
    */
-  public function get()
+  public function get($data)
   {
+    if (is_int($data)) {
+      $query = $this->db->query('SELECT * FROM practiseCRUD WHERE id = ' . $data);
+      $output = $query->fetch(PDO::FETCH_ASSOC);
 
+      return new Task($output);
+    }
   }
 
   /**
    * Return all tasks
+   *
+   * @return Array
    */
-  public function getAll()
+  public function getTasks()
   {
+    $query = $this->db->query('SELECT * FROM practiseCRUD');
 
+    $tasks = [];
+
+    while ($data = $query->fetch(PDO::FETCH_ASSOC)) {
+      $tasks[] = new Task($data);
+    }
+
+    return $tasks;
+  }
+
+  /**
+   * Return all categories
+   *
+   * @return Array
+   */
+  public function getCategories()
+  {
+    $query = $this->db->query('SELECT category FROM practiseCRUD');
+
+    $categories = [];
+
+    while ($data = $query->fetch()) {
+      $categories[] = $data['category'];
+    }
+
+    return $categories;
   }
 }
